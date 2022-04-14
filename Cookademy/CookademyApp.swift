e7ad6a2448a00e659e27345e0b8130046018bb0a
Esteban Calvete <esteban.calvete@gmail.com>
2022-04-14 10:20:09 +0200

    Initial Commit

---
+++ Cookademy/CookademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookademyApp.swift
+//  Cookademy
+//
+//  Created by Esteban Calvete Iglesias on 14/4/22.
+//
+
+import SwiftUI
+
+@main
+struct CookademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
