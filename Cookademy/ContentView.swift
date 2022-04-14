d0a415bed6b75a8be79c85c407dc1eda1d1b1b8c
Esteban Calvete <esteban.calvete@gmail.com>
2022-04-14 12:57:22 +0200

    First commit

--- Cookademy/ContentView.swift
+++ Cookademy/ContentView.swift
@@ -8,8 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    let myIngredient = Ingredient(name: "Sugar", quantity: 2.0, unit: .tbs)
+    let mySecondIngredient = Ingredient(name: "Salt", quantity: 1.0, unit: .tsp)
+    let myThirdIngredient = Ingredient(name: "Apple", quantity: 2.0, unit: .none)
+    let myFourthIngredient = Ingredient(name: "Banana", quantity: 1.0, unit: .none)
+    
     var body: some View {
-        Text("Hello, world!")
+        Text(myIngredient.description)
+            .padding()
+        Text(mySecondIngredient.description)
+            .padding()
+        Text(myThirdIngredient.description)
+            .padding()
+        Text(myFourthIngredient.description)
             .padding()
     }
 }
